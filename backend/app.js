afad1bf4bae11e639af504a7e258befd74073578
Rodrigo Rangel <rodrigorangeldev@gmail.com>
2020-12-02 09:06:05 -0300

    retirando dados sensiveis

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const nodemailer = require('nodemailer')
 
 
 require('./model/orcamento')
@@ -8,7 +9,7 @@ const Orcamento = mongoose.model('Orcamento')
 const app = express()
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://rrarangel:subzero@cluster0-pzqsd.gcp.mongodb.net/celke?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true', { 
+mongoose.connect('conexao', { 
    useNewUrlParser: true,
    useUnifiedTopology: true
 })
@@ -30,6 +31,30 @@ app.post('/orcamento', async (req, res) => {
             message: 'Erro ao enviar os dados: ' + err
          })
       }
+
+      var transport = nodemailer.createTransport({
+         host: 'smtp.mailtrap.io',
+         port: 2525,
+         auth: {
+            user: '05bb5553d09d2d',
+            pass: '227f9bbccd33f3'
+         }
+      })
+
+      var sendInfo = {
+            from: '56674090b6-94d7f2@inbox.mailtrap.io', // sender address
+            to: req.body.email, // list of receivers
+            subject: "Solicitação de orçamento recebida. ✔", // Subject line
+            text: "Prezado(a)\n\n Recebi sua solicitação de orçamento em breve entrarei em contato.", // plain text body
+            html: "<b>Prezado(a)</b><br /><br /> Recebi sua solicitação de orçamento em breve entrarei em contato.", // html body
+          
+      }
+
+      await transporter.sendMail(sendInfo, function(err){
+         if(err){
+            console.log('Erro ao enviar o email: ' +err)
+         }
+      });
 
       return res.json({
          error: false,
